a571a4c50966e24edc4dfb718cd6a65fc016c6cf
Robert A. dos Anjos <78178196+ohntrebor@users.noreply.github.com>
2024-09-30 18:00:33 -0300

    x

--- provider.tf
+++ provider.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1" # Ajuste conforme a sua região preferida
 }
 
+
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   token                  = data.aws_eks_cluster_auth.cluster.token
