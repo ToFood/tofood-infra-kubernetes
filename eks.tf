797129d42762ffe1b99ce40c6f202fc035b4e00a
Robert A. dos Anjos <78178196+ohntrebor@users.noreply.github.com>
2024-11-02 19:31:31 -0300

    🔧

--- eks.tf
+++ eks.tf
@@ -1,37 +1,43 @@
-resource "aws_eks_cluster" "tofood_cluster" {
-  name     = "tofood-eks-cluster"
-  role_arn = aws_iam_role.eks_role.arn
-
-  vpc_config {
-    subnet_ids = aws_subnet.tofood_subnets[*].id
+resource "aws_eks_node_group" "tofood_node_group" {
+  cluster_name    = aws_eks_cluster.tofood_cluster.name
+  node_group_name = "tofood-node-group"
+  node_role_arn   = aws_iam_role.eks_node_role.arn
+  subnet_ids      = aws_subnet.tofood_subnets[*].id
+  scaling_config {
+    desired_size = 2
+    max_size     = 3
+    min_size     = 1
   }
+  instance_types = ["t3.medium"]
 
   tags = {
-    Name = "tofood-eks-cluster"
+    Name = "tofood-node-group"
   }
 }
 
-resource "aws_iam_role" "eks_role" {
-  name = "tofood-eks-role"
+resource "aws_iam_role" "eks_node_role" {
+  name = "tofood-eks-node-role"
 
-  assume_role_policy = data.aws_iam_policy_document.eks_assume_role_policy.json
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
 }
 
-data "aws_iam_policy_document" "eks_assume_role_policy" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["eks.amazonaws.com"]
-    }
-  }
+resource "aws_iam_role_policy_attachment" "eks_node_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = aws_eks_cluster.tofood_cluster.name
-}
-
-data "aws_eks_cluster_auth" "cluster" {
-  name = aws_eks_cluster.tofood_cluster.name
+resource "aws_iam_role_policy_attachment" "eks_cni_policy" {
+  role       = aws_iam_role.eks_node_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSCNIPolicy"
 }
