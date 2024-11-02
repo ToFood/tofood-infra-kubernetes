eae796c3c6fbe9b1e2de4f24f8da20fdb63f613e
Robert A. dos Anjos <78178196+ohntrebor@users.noreply.github.com>
2024-11-02 16:36:58 -0300

    x

--- vpc.tf
+++ vpc.tf
@@ -6,9 +6,9 @@ resource "aws_vpc" "tofood_vpc" {
 }
 
 resource "aws_subnet" "tofood_subnets" {
-  count = 3
-  vpc_id = aws_vpc.tofood_vpc.id
-  cidr_block = "10.0.${count.index}.0/24"
+  count             = 3
+  vpc_id            = aws_vpc.tofood_vpc.id
+  cidr_block        = "10.0.${count.index}.0/24"
   availability_zone = data.aws_availability_zones.available.names[count.index]
   tags = {
     Name = "tofood-subnet-${count.index}"
